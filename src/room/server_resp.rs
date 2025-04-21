7ebaca89261217e43286658a5877fcf0d2b77ace
Eucalypt <eluvk.dev@gmail.com>
2025-04-21 11:26:17 +0800

    v0.0.6

--- src/room/server_resp.rs
+++ src/room/server_resp.rs
@@ -12,7 +12,7 @@ pub enum ServerResp {
 
 impl ServerResp {
     pub fn auth_success_version() -> Self {
-        Self::Version("0.0.5".to_string())
+        Self::Version("0.0.6".to_string())
     }
 
     pub fn rejoin_room(room_id: String) -> Self {
